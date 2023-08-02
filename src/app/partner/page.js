e21af50ce71483e31871991e4194bb938fa7f4ae
Nox <patrik.tuominen@outlook.com>
2023-08-03 02:50:41 +0300

    🚧 feat: page.js

---
+++ src/app/partner/page.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Header from '../../components/header';
+
+export default function Page() {
+    return (
+      <>
+        <Head>
+            <title>Rainbow Creators</title>
+        </Head>
+        <Header subTitle="Partner program is made for you"/>
+        <main>
+          <section>
+            <article>
+                <h1>Partner program is our way of giving you your own customizable space in our page.</h1>
+                <p>
+                    Even though the it is called "Partner program", we want to think of it as symbiosis. Giving you the space to promote your love of this community and to connect with people alike. As our partner you get full control over your little space on our page, able to connect your Discord community and other fun stuff!
+                </p>
+            </article>
+          </section>
+        </main>
+      </>
+    );
+  }
