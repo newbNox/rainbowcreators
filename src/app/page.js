6f5d8a4ab3e7cf7cc8fc70d991887b64cb4fbe3a
Nox <patrik.tuominen@outlook.com>
2023-08-03 02:41:53 +0300

    🎨 feat: Moved stuff under src/

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Header from '../components/header';
+
+export default function Page() {
+    return (
+      <>
+        <Head>
+            <title>Rainbow Creators</title>
+        </Head>
+        <Header subTitle="The landing page of all LGBTQIA+ content creators"/>
+        <main>
+          <section>
+            <article>
+              
+            </article>
+          </section>
+        </main>
+      </>
+    );
+  }
