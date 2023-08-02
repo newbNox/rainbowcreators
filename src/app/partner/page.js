d5f6c791641c4c54cfca6f39adca6ad8bfb01317
Nox <patrik.tuominen@outlook.com>
2023-08-03 02:59:35 +0300

    ✏️ fix: Fix correct title

--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     return (
       <>
         <Head>
-            <title>Rainbow Creators</title>
+            <title>Rainbow Creators : Partner program</title>
         </Head>
         <Header subTitle="Partner program is made for you"/>
         <main>
