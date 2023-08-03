d07ed81444936894dc3da98699942b48b0e5e4c8
Nox <patrik.tuominen@outlook.com>
2023-08-04 00:29:08 +0300

    ✨ refactor: Footer with links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
@@ -6,10 +8,13 @@ const Footer = () => {
             <div className='container'>
                 <div className='row pt-4'>
                     <div className='col'>
-                        <p>Col1</p>
+                        <ul class="list-group-flush">
+                            <li class="list-group-item"><a href="https://github.com/newbNox/rainbowcreators" target="_blank" className='light'><FontAwesomeIcon icon={faGithub} /> Open Source project by newbNox</a></li>
+                            <li class="list-group-item"><a href="https://discord.gg/AtsPhp3Wk2" target='_blank' className='light'><FontAwesomeIcon icon={faDiscord}/> Discord Community</a></li>
+                        </ul>
                     </div>
                     <div className='col'>
-                        <p>Col1</p>
+                        
                     </div>
                 </div>
             </div>
