51ff7f03196f6e61ad262aef0f37ac71319c61b3
korhox <juuso.korhonen@bittivirta.fi>
2023-08-03 21:03:13 +0300

    feat: :sparkles: Add FA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import "./css/global.scss"
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default function RootLayout({ children }) {
     return (
