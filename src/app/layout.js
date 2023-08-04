fdfe9c387950010def48fbd8e8ed1531b31d234d
Nox <patrik.tuominen@outlook.com>
2023-08-04 04:18:30 +0300

    ✨ feat: Added NextAuth: Twitch login

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react"
 import "./css/global.scss"
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -11,8 +12,10 @@ config.autoAddCss = false
 
 export default function RootLayout({ children }) {
     return (
+        <SessionProvider session={session}>
         <html lang="en" className={roboto.className}>
             <body>{children}</body>
         </html>
+        </SessionProvider>
     )
 }
