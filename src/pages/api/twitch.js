b319c72b133abe38e9ed1e9902b516842533adc2
Nox <patrik.tuominen@outlook.com>
2023-08-06 04:45:24 +0300

    💥 feat: Actually filtering tags from streams

--- src/pages/api/twitch.js
+++ src/pages/api/twitch.js
@@ -24,17 +24,63 @@ export default async function handler(req, res) {
 
     const tag = req.query.tag; // Assuming you pass the tag as a query parameter
 
-    const twitchUrl = `https://api.twitch.tv/helix/streams?tag_id=${tag}&first=12&language=en&type=live`;
-    const twitchResponse = await fetch(twitchUrl, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Client-ID': TWITCH_CLIENT_ID,
-      },
-    });
+    const limit = 12; // Number of streams per page
+    let cursor = null; // Initialize the cursor to null
+    let fetchedStreams = 0; // Number of streams fetched so far
+    const fetchedCursors = []
 
-    const twitchJson = await twitchResponse.json();
+    // Array to store all fetched streams
+    let allStreams = [];
 
-    res.status(200).json(twitchJson);
+    // Fetch streams with pagination until we reach the desired limit
+    while (fetchedStreams < limit) {
+      if (cursor && fetchedCursors.includes(cursor)) {
+        // Skip fetching this cursor again
+        break;
+      }
+      let twitchUrl = `https://api.twitch.tv/helix/streams?first=${limit - fetchedStreams}&language=en`;
+      if (cursor) {
+        twitchUrl += `&after=${cursor}`;
+      }
+
+      const twitchResponse = await fetch(twitchUrl, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Client-ID': TWITCH_CLIENT_ID,
+        },
+      });
+
+      const twitchJson = await twitchResponse.json();
+
+      // Check if the response contains any streams
+      if (twitchJson.data.length === 0) {
+        break; // No more streams to fetch, exit the loop
+      }
+
+      // Filter the fetched streams to include only streams with the desired tag
+      const streamsWithTag = twitchJson.data.filter(stream => {
+        return Array.isArray(stream.tags) && stream.tags.includes(tag);
+      });
+
+      // Add the filtered streams to the 'allStreams' array
+      allStreams = allStreams.concat(streamsWithTag);
+
+      // Update the fetchedStreams count
+      fetchedStreams += streamsWithTag.length;
+
+      if (cursor) {
+        fetchedCursors.push(cursor);
+      }
+      // Update the cursor to fetch the next page
+      cursor = twitchJson.pagination.cursor;
+      console.log(fetchedCursors)
+      console.log(allStreams.length)
+    }
+                                                
+    const finalStreams = allStreams.slice(0, limit);
+    //console.log(streams);
+
+    res.status(200).json(finalStreams);
   } catch (error) {
     console.error('Error fetching Twitch data:', error);
     res.status(500).json({ error: 'Internal server error' });
