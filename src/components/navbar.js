754e72f41cd6a4dd4b2a22c7e83348fc0020525b
Nox <patrik.tuominen@outlook.com>
2023-08-09 17:34:43 +0300

    ✨ feat: Added community servers to nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faServer, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
     return (
@@ -7,6 +7,7 @@ const Nav = () => {
             <header class="d-flex justify-content-center py-3">
                 <ul class="nav nav-fill">
                     <li class="nav-item btn btn-danger mx-2"><a href="/" class="nav-link"><FontAwesomeIcon icon={faHouse}/> Home</a></li>
+                    <li class="nav-item btn btn-danger mx-2"><a href="/servers" class="nav-link"><FontAwesomeIcon icon={faServer}/> Community Servers</a></li>
                     <li class="nav-item btn btn-danger mx-2"><a href="partner" class="nav-link"><FontAwesomeIcon icon={faUsers}/> Partnership</a></li>
                 </ul>
             </header>
