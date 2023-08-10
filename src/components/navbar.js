da57b9a98db7e2b1d43dbc013badce589986efeb
Nox <patrik.tuominen@outlook.com>
2023-08-10 14:17:35 +0300

    💄 feat: Add margin for mobile devices

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ const Nav = () => {
         <div class="container mt-5">
             <header class="d-flex justify-content-center py-3">
                 <ul class="nav nav-fill">
-                    <li class="nav-item btn btn-danger mx-2"><a href="/" class="nav-link"><FontAwesomeIcon icon={faHouse}/> Home</a></li>
-                    <li class="nav-item btn btn-danger mx-2"><a href="/servers" class="nav-link"><FontAwesomeIcon icon={faServer}/> Community Servers</a></li>
-                    <li class="nav-item btn btn-danger mx-2"><a href="partner" class="nav-link"><FontAwesomeIcon icon={faUsers}/> Partnership</a></li>
+                    <li class="nav-item btn btn-danger mx-2 my-2"><a href="/" class="nav-link"><FontAwesomeIcon icon={faHouse}/> Home</a></li>
+                    <li class="nav-item btn btn-danger mx-2 my-2"><a href="/servers" class="nav-link"><FontAwesomeIcon icon={faServer}/> Community Servers</a></li>
+                    <li class="nav-item btn btn-danger mx-2 my-2"><a href="partner" class="nav-link"><FontAwesomeIcon icon={faUsers}/> Partnership</a></li>
                 </ul>
             </header>
         </div>
