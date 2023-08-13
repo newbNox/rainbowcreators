2842aedb467d98edb6ee3ac3372e90c3d2da567d
Nox <patrik.tuominen@outlook.com>
2023-08-13 03:24:48 +0300

    ✨ feat: Added BMC link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
+import buyCoffee from '../public/bmc-button.png';
 
 const Footer = () => {
     return (
@@ -13,8 +15,10 @@ const Footer = () => {
                             <li className="list-group-item"><a href="/discord" target='_blank' className='light'><FontAwesomeIcon icon={faDiscord}/> Discord Community</a></li>
                         </ul>
                     </div>
-                    <div className='col'>
-                        
+                    <div className='col text-end'>
+                        <ul className='list-group-flush'>
+                            <li className='list-group-item'><a href='https://www.buymeacoffee.com/rainbowcreators' target='_blank'><Image src={buyCoffee} className='buyCoffee'/></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
